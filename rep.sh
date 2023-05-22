9bd1efd80844bd075389ff9323fc44a06ce82e1f
blschachte <benjaminschachte@gmail.com>
2023-05-21 20:04:56 -0400

    automate

--- rep.sh
+++ rep.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
 set -e
-# insert modified files into config/
+
+echo "-- Updating Host System"
+apt update && apt upgrade -y
+
+echo "-- Entering Build Directory"
+cd build
+echo "-- Preparing Build Directory"
+ lb clean
+echo "-- Creating Config Folder"
+lb config
+echo "-- Leaving Build Directory"
+cd ..
+echo "-- Modifying Config Folder"
+#insert modified files into config/
 cp -r files-modified/* build/config/includes.chroot_after_packages/ && echo "copied files to build/config/includes.chroot_after_packages/"
 cp -r files-modified/* build/config/includes.chroot_before_packages/ && echo "copied files to build/config/includes.chroot_before_packages/"
 cp -r hooks/* build/config/hooks/live/ && echo "copied hooks to build/config/hooks/live/"
 cp -r hooks/* build/config/hooks/normal/ && echo "copied hooks to build/config/hooks/normal"
+echo "-- Entering Build Directory"
+cd build
+echo "-- Building image"
+lb build
+cd build
+echo "-- Writing image to the disk"
+dd if=pennyweight-linux-amd64.hybrid.iso of=$1 status=progress
+echo "Exiting Build Directory"
+cd ..
+echo "-- Done"
